04df2cb391ef8601f8f527ef3bb9f2763f866150
marla729 <carcar2014m@gmail.com>
2022-08-07 20:17:48 -0400

    init spring boot

---
+++ src/main/java/home/clinic/simplier/SimplierApplication.java
@@ -0,0 +1,13 @@
+package home.clinic.simplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/home/clinic/simplier/SimplierApplicationTests.java
@@ -0,0 +1,13 @@
+package home.clinic.simplier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
